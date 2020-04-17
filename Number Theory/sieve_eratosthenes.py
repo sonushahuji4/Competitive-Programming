d4a9be77cd1ad875d8e6a118cfd82f8088bcb54a
shahuji <sonushahuji4@gmail.com>
2020-04-17 18:42:38 +0530

    updating Sieve Of Eratosthenes Algo for finding prime numbers

--- Number Theory/sieve_eratosthenes.py
+++ Number Theory/sieve_eratosthenes.py
@@ -2,12 +2,15 @@
 # Time Complexity : Nlog(log(N))
 # 0 => represents composite number
 # 1 => represents prime number
-n = 15
-is_prime = [1 for i in range(n)]
-is_prime[0] = is_prime[1] = 0
 
-for i in range(2,int(n**0.5)+1):
-	if is_prime[i]:
-		for j in range(i*i,n,i):
-			is_prime[j] = 0
-print(*is_prime)
+def sieve(n):
+	is_prime = [1 for i in range(n)]
+	is_prime[0] = is_prime[1] = 0
+	for i in range(2, int(n ** 0.5) + 1):
+		if is_prime[i]:
+			for j in range(i * i, n, i):
+				is_prime[j] = 0
+	return is_prime
+
+
+print(*sieve(15))
