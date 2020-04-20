20724a5a0426e1dd06954ba4c72e122f29c7195f
shahuji <sonushahuji4@gmail.com>
2020-04-20 21:13:51 +0530

    practice problem

---
+++ Number Theory/Practice Problems/Divisor_Summation.py
@@ -0,0 +1,16 @@
+# Problem Statement Link : https://www.spoj.com/problems/DIVSUM/
+
+# Solution :
+
+
+for _ in range(int(input())):
+	n = int(input())
+	ans = 0
+	i = 1
+	while i*i < n:
+		if n % i == 0:
+			ans += i+n//i
+		i += 1
+	if i*i == n:
+		ans += i
+	print(ans-n)
