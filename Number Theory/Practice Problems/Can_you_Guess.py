7744b37e39963bb9df8bad591dd0cdffa843ad35
shahuji <sonushahuji4@gmail.com>
2020-04-28 20:10:00 +0530

    Can you guess

--- Number Theory/Practice Problems/Can_you_Guess.py
+++ Number Theory/Practice Problems/Can_you_Guess.py
@@ -1,9 +1,11 @@
 from functools import reduce
 
+
 def factors(n):
 	return set(reduce(list.__add__, ([i, n // i] for i in range(1, int(n ** 0.5) + 1) if n % i == 0)))
+
 
 for _ in range(int(input())):
 	n = int(input())
 	data = factors(n)
-	print(sum(data)-n)
+	print(sum(data) - n)
