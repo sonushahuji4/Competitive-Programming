c41c924147fb20cc99192c46a079556f3107d4e0
shahuji <sonushahuji4@gmail.com>
2020-04-28 21:20:42 +0530

    Counting number of set bits using right shift

---
+++ Bit Manipulation/Counting_Number_Of_Set_Bits.py
@@ -0,0 +1,39 @@
+# Reference Video Tutorial Link : https://www.youtube.com/watch?v=_o7QBzM33J0&list=PL2q4fbVm1Ik7ip1VkWwe5U_CEb93vw6Iu&index=4
+
+# Given : A number N and find number of set(1's) bits in it.
+# Example :
+# 			N = 12 => (1 1 0 0)  => OUTPUT : 2
+# 			N = 11 => (1 0 1 1)  => OUTPUT : 3
+#
+# Solution :  We will be using right shift to find total set not bits
+#
+# 			N = 0 1 0 0 1 1 1 0
+#
+# OPERATIONS : (and bit operation)    N & 1
+#
+# 				0 1 0 0 1 1 1 0
+# 				0 0 0 0 0 0 0 1
+# 			-----------------------
+# 				0 0 0 0 0 0 0 0     here : 0 & 1 = 0, so we do not increament count
+# 			-----------------------
+#
+# 			: (Right shift operation) N >> 1
+#
+# 			N = 0 0 1 0 0 1 1 1
+#
+# 				0 0 1 0 0 1 1 1
+# 				0 0 0 0 0 0 0 1
+# 			-----------------------
+# 				0 0 0 0 0 0 0 1     here : 0 & 1 = 1, so we increament count by 1
+# 			-----------------------
+# Repeat until N becomes 0
+
+# Time Complexity : O(Log(N))
+for _ in range(int(input())):
+	n = int(input())
+	ans = 0
+	while n > 0:
+		if (n & 1) > 0:
+			ans += 1
+		n = n >> 1  # at some point n becomes zero,
+	print(ans)
