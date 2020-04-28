e32896abfe44026ba6f87cf92f693846c695b0d7
shahuji <sonushahuji4@gmail.com>
2020-04-28 21:40:01 +0530

    Counting number of set bits using right shift modified Approach

--- Bit Manipulation/Counting_Number_Of_Set_Bits.py
+++ Bit Manipulation/Counting_Number_Of_Set_Bits.py
@@ -1,6 +1,9 @@
 # Reference Video Tutorial Link : https://www.youtube.com/watch?v=_o7QBzM33J0&list=PL2q4fbVm1Ik7ip1VkWwe5U_CEb93vw6Iu&index=4
 
 # Given : A number N and find number of set(1's) bits in it.
+
+# ------------------------------------------------------| Approach  1 |------------------------------------------------------
+
 # Example :
 # 			N = 12 => (1 1 0 0)  => OUTPUT : 2
 # 			N = 11 => (1 0 1 1)  => OUTPUT : 3
@@ -36,4 +39,36 @@ for _ in range(int(input())):
 		if (n & 1) > 0:
 			ans += 1
 		n = n >> 1  # at some point n becomes zero,
+	print(ans)
+
+# ------------------------------------------------------| Approach  2 |------------------------------------------------------
+
+# Observation :
+# 				8 - 1 = 7
+#
+# 			8 ==>     0 1 0 0 0     here => when u subtract N - 1, in binary representation  after 1, it makes all number 1's
+# 			1 ==>     0 0 0 0 1
+# 					-
+# 					--------------
+# 					   0 0 1 1 1    <== after subtracting N - 1
+# 					--------------
+# 			similary
+#
+# 			16 - 1 = 15
+#
+# 			16 ==>     1 0 0 0 0     here => when u subtract N - 1, in binary representation  after 1, it makes all number 1's
+# 			1  ==>     0 0 0 0 1
+# 					-
+# 					--------------
+# 					   0 1 1 1 1    <== after subtracting N - 1
+# 					--------------
+
+# Time Complexity : O(Log(N))
+
+for _ in range(int(input())):
+	n = int(input())
+	ans = 0
+	while n > 0:
+		ans += 1
+		n = n & (n - 1)
 	print(ans)
