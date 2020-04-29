52095dfb89ccb76ac385df5c09cc72539c312f01
shahuji <sonushahuji4@gmail.com>
2020-04-29 10:09:16 +0530

    Shubham and Binary String

---
+++ Bit Manipulation/Subham_and_Binary_Strings.py
@@ -0,0 +1,7 @@
+# Problem Statement Link : https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/practice-problems/algorithm/subham-and-binary-strings/
+
+for _ in range(int(input())):
+	n = int(input())
+	binaryData = list(input())
+	ans = binaryData.count('0')
+	print(ans)
